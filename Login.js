34a3390ea6f1b291ab1a2f2ff299df752d37cc7c
sportsrehabiliationcoach <sportsrehab04@gmail.com>
2024-08-04 20:16:24 +0530

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,25 +1,16 @@
 import React, { useState } from 'react'
 import "./Login.css"
 import { Link, useNavigate } from "react-router-dom"
-
-
 function Login() {
-
   const [details, setDetails] = useState({ username: "", password: "" });
   const [showPassword, setshowPassword] = useState(false);
   const navigate = useNavigate();
-
   const handleClick = async (e) => {
     e.preventDefault();
-
-
-
-    if (!details.username || !details.password) {
+  if (!details.username || !details.password) {
       alert("Fill your credentials to login!");
       return;
     }
-
-
     try {
       const response = await fetch('http://localhost:5000/api/login', {
         method: 'POST',
@@ -45,17 +36,9 @@ function Login() {
   const showConfirmPassword = () => {
     setshowPassword(!showPassword);
   }
-
-
-
-  const onChange = (event) => {
+ const onChange = (event) => {
     setDetails({ ...details, [event.target.name]: event.target.value })
   }
-
-
-
-
-
   return (
     <div className="login-page">
       <form className="login" onSubmit={handleClick}>
@@ -66,7 +49,6 @@ function Login() {
         <div className="form-inner">
           <h2>Enter your credentials!</h2>
           <div className="container">
-
            <input className='input' type="text" placeholder='username' name="username" value={details.username} onChange={onChange} />
             <div className='toggle'>
               <input className='input' type={showPassword?"text":"password"} placeholder='password' name="password" value={details.password} onChange={onChange} />
@@ -74,26 +56,14 @@ function Login() {
                 {showPassword ? '🙈' : '👁️'}
               </span>
             </div>
-
-
             <h4 class="forgot">Forgot password</h4>
             <div className='btn'>
               <button type="submit" className="log1">Login</button>
               <Link to='/signin'><button type="button" className="log2">REGISTER</button></Link>
             </div>
-
-
-
-
-
-
-
           </div>
         </div>
-
-
       </form>
-
     </div>
   )
 };
